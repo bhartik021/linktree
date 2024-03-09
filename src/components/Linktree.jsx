360ec2c1dc559645966d4ab6f7277451ace93268
Bharti Kumari <bhartik021@gmail.com>
2024-03-09 19:50:44 +0530

    layout completed

--- src/components/Linktree.jsx
+++ src/components/Linktree.jsx
@@ -65,6 +65,20 @@ const Linktree = () => {
           </div>
         </div>
       </div>
+
+      <div className="flex flex-row justify-center text-center max-w-full">
+        <div className="relative leading-[24px] z-[1]">
+          {`Made with ♥ `}
+          <a
+            className="text-[inherit]"
+            href="https://www.bhartikumari.in/"
+            target="_blank"
+          >
+            <span className="[text-decoration:underline]">Bharti Kumari</span>
+          </a>
+        </div>
+      </div>
+      
     </div>
   );
 };

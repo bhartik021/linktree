2aad64ae939edd472569af91a0716f82c855d232
Bharti Kumari <bhartik021@gmail.com>
2024-03-11 02:01:01 +0530

    reupdated

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Linktree from './components/Linktree.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document``.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <App />
